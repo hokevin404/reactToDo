e6e1d39a632982986f6e8f04415498924b66ff4f
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-15 08:28:59 -0700

    Mockup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,33 @@ function App() {
 
   return (
     <>
-      
+      <div className='title'>
+        <h2>Create To-Do List</h2>
+      </div>
+      <div className='todoAddContainer'>
+        <input type='text' name='todoAdd' className='todoAdd' placeholder='Add Task' />
+        <input type='submit' value="submit" />
+      </div>
+      <ul className='appList'>
+        <li>
+          <input type="checkbox" className="items" name="item1" value="Item1" />
+          <label htmlFor="item1"> Item1</label>
+          <button className='editButton'> Edit</button>
+          <button className='deleteButton'> Delete</button>
+        </li>
+        <li>
+          <input type="checkbox" className="items" name="item2" value="Item2" />
+          <label htmlFor="item2"> Item2</label>
+          <button className='editButton'> Edit</button>
+          <button className='deleteButton'> Delete</button>
+        </li>
+        <li>
+          <input type="checkbox" className="items" name="item3" value="Item3" />
+          <label htmlFor="item3"> Item3</label>
+          <button className='editButton'> Edit</button>
+          <button className='deleteButton'> Delete</button>
+        </li>
+      </ul>
     </>
   )
 }
